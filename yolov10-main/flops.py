06994f8d615054a4c3df490ca39da4dd674d2345
ziran001 <zebra00229@gmail.com>
2025-11-09 22:34:56 +0800

    Rename YOLO-FRP config and add training script

--- yolov10-main/flops.py
+++ yolov10-main/flops.py
@@ -1,6 +1,6 @@
 from ultralytics import YOLOv10
 
-model = YOLOv10('yolov10n.yaml')
+model = YOLOv10('yolov10-frp.yaml')
 model.model.model[-1].export = True
 model.model.model[-1].format = 'onnx'
 del model.model.model[-1].cv2
